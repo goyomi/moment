31508929ca95a94cc9f29a83ffa75018536f3930
goyomi <ghdl1004797@gmail.com>
2024-03-03 21:41:58 +0900

    Fix: 시간 한 자리 표기 >> 두 자리 표기

--- dateTime.js
+++ dateTime.js
@@ -15,9 +15,9 @@ function getDate() {
 
 function getTime() {
   const date = getDate();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
 
   clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
