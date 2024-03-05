ede360661d8ffb7210be84a2fb65efd7ae889c22
goyomi <ghdl1004797@gmail.com>
2024-03-05 18:17:20 +0900

    Feat: 재 로그인시 input에 사용했던 user name 남기기

--- logout.js
+++ logout.js
@@ -1,6 +1,10 @@
 const logoutBtn = document.querySelector('#logoutBtn');
 
 function onLogoutBtnClick() {
+  // log in 후 뒤로가기 버튼 눌렀을 때 userName 남기기
+  const userNameInput = loginForm.querySelector('input');
+  userNameInput.value = localStorage.getItem(USER_NAME_KEY);
+
   localStorage.removeItem(USER_NAME_KEY);
 
   header.style.display = 'none';
