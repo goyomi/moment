510fb52af396432d334bdcf27917350b255d439b
goyomi <ghdl1004797@gmail.com>
2024-03-13 01:49:13 +0900

    Style: UI 변경에 따른 수정

--- login.js
+++ login.js
@@ -1,20 +1,20 @@
-const loginForm = document.querySelector('#loginForm');
-const loginInput = loginForm.querySelector('input');
-const loginUserName = document.querySelector('#loginUserName');
-const header = document.querySelector('header');
-const main = document.querySelector('main');
-const footer = document.querySelector('footer');
+const loginForm = document.querySelector("#loginForm");
+const loginInput = loginForm.querySelector("input");
+const loginUserName = document.querySelector("#loginUserName");
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
 
-const HIDE_CLASSNAME = 'hide_display';
-const USER_NAME_KEY = 'userName';
+const HIDE_CLASSNAME = "hide_display";
+const USER_NAME_KEY = "userName";
 
 function onLoginSubmit(event) {
   event.preventDefault();
 
   // 공백제출 막기
   const trimLoginInput = loginInput.value.trim();
-  if (trimLoginInput === '') {
-    alert('이름을 한 글자 이상 입력해 주세요.');
+  if (trimLoginInput === "") {
+    alert("이름을 한 글자 이상 입력해 주세요.");
     return;
   }
 
@@ -22,24 +22,24 @@ function onLoginSubmit(event) {
   localStorage.setItem(USER_NAME_KEY, userName);
   loginForm.classList.add(HIDE_CLASSNAME);
 
-  header.style.display = '';
-  main.style.display = '';
-  footer.style.display = '';
+  header.style.display = "";
+  main.style.display = "";
+  footer.style.display = "";
   loginUserName.innerText = userName;
 
-  loginInput.value = '';
+  loginInput.value = "";
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
+loginForm.addEventListener("submit", onLoginSubmit);
 
 const getUserName = localStorage.getItem(USER_NAME_KEY);
 if (getUserName === null) {
   loginForm.classList.remove(HIDE_CLASSNAME);
 } else {
-  // 로그인 후 
+  // 로그인 후
   loginForm.classList.add(HIDE_CLASSNAME);
-  header.style.display = '';
-  main.style.display = '';
-  footer.style.display = '';
-  loginUserName.innerText = getUserName;
+  header.style.display = "";
+  main.style.display = "";
+  footer.style.display = "";
+  loginUserName.innerText = `${getUserName} 님.`;
 }
