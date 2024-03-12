d67f0011e9ddc85246fd07110336e67604510ddf
goyomi <ghdl1004797@gmail.com>
2024-03-13 00:43:44 +0900

    Feat: 시간 표시 AM/Pm으로 변경

--- dateTime.js
+++ dateTime.js
@@ -1,12 +1,12 @@
-const days = document.querySelector('#days');
-const clock = document.querySelector('#clock');
+const days = document.querySelector("#days");
+const clock = document.querySelector("#clock");
 
 // 날짜 & 시간
 function getDate() {
   const date = new Date();
   const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
 
   days.innerText = `${year}-${month}-${day}`;
 
@@ -15,11 +15,15 @@ function getDate() {
 
 function getTime() {
   const date = getDate();
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
+  let hours = date.getHours();
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  const ampm = hours >= 12 ? "PM" : "AM";
 
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  hours = hours % 12;
+  hours = hours ? hours.toString().padStart(2, "0") : "12";
+
+  clock.innerText = `${hours}:${minutes}:${seconds} ${ampm}`;
 }
 
 getDate();
